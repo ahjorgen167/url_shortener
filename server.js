e7f61336e010ea5e9b8de3f70f046ab821cef2a2
ahjorgen167 <ahjorgen167@gmail.com>
2017-01-22 19:49:06 -0800

    Attempt to resolve security issue

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 var express = require('express')
 var passport = require('passport');
+
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var bodyParser = require('body-parser');
+var session      = require('express-session');
+
 var app = express()
 
-//var csurf = require('csurf')
+app.use(cookieParser());
+app.use(session({ secret: "process.env.SESSION_SECRET" }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-//app.use(cookieParser());
-//app.use(session({ secret: "process.env.SESSION_SECRET" }));
-//app.use(csurf());
-
-// If using passport. Used in mean.io/meanjs
-
-//app.use(function(req, res, next) {
-//  res.cookie('XSRF-TOKEN', req.csrfToken());
-//  next();
-//});
-
-//app.use(passport.initialize());
-//app.use(passport.session());
-
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -32,4 +23,4 @@ require ("./server/app.js")(app);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port') + "\nStarting At:\n" + new Date().toString());
-})
+}) 
