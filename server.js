374d7d55ac7053b98b49e1930deffbdbebb5163e
ahjorgen167 <ahjorgen167@gmail.com>
2017-01-22 19:40:59 -0800

    fixing security issues

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ var csurf = require('csurf')
 
 
 app.use(cookieParser());
-app.use(session({ secret: "process.env.SESSION_SECRET" }));
-app.use(csurf());
+//app.use(session({ secret: "process.env.SESSION_SECRET" }));
+//app.use(csurf());
 
 // If using passport. Used in mean.io/meanjs
 
-app.use(function(req, res, next) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  next();
-});
+//app.use(function(req, res, next) {
+//  res.cookie('XSRF-TOKEN', req.csrfToken());
+//  next();
+//});
 
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
